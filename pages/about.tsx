13e436adb5747bc67c3f562911577994d529bc60
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2022-06-24 10:32:43 +0200

    First commit

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import Link from 'next/link'
+
+const About: NextPage = () => {
+  return (
+    <div>
+      Aboutt
+      <br />
+      <Link href="/">To home</Link>
+    </div>
+  )
+}
+
+export default About
