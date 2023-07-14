82c9d8e5907b443665692aff12985c3bc6ea605a
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-07-14 12:46:32 +0200

    Updated font family loading

--- src/configs/theme.config.ts
+++ src/configs/theme.config.ts
@@ -2,19 +2,65 @@ import { deepmerge } from '@mui/utils'
 import { createTheme } from '@mui/material'
 import { theme as muiItaliaTheme } from '@pagopa/mui-italia'
 
+import { Titillium_Web } from 'next/font/google'
+
+const titilliumWeb = Titillium_Web({ subsets: ['latin'], weight: ['300', '400', '600', '700'] })
+const fontFamily = titilliumWeb.style.fontFamily
+
 /**
  * It is based on the `@pagopa/mui-italia` theme.
  * It overrides some of the default values.
  */
 export const theme = createTheme(
-  deepmerge(
-    {
-      components: {
-        MuiTypography: {
-          styleOverrides: { root: { wordBreak: 'break-word' } },
-        },
+  deepmerge(muiItaliaTheme, {
+    typography: {
+      fontFamily,
+      h1: {
+        fontFamily,
+      },
+      h2: {
+        fontFamily,
+      },
+      h3: {
+        fontFamily,
+      },
+      h4: {
+        fontFamily,
+      },
+      h5: {
+        fontFamily,
+      },
+      h6: {
+        fontFamily,
+      },
+      subtitle1: {
+        fontFamily,
+      },
+      subtitle2: {
+        fontFamily,
+      },
+      body1: {
+        fontFamily,
+      },
+      body2: {
+        fontFamily,
+      },
+      button: {
+        fontFamily,
+      },
+      caption: {
+        fontFamily,
+      },
+      overline: {
+        fontFamily,
       },
     },
-    muiItaliaTheme
-  )
+    components: {
+      MuiTypography: {
+        styleOverrides: { root: { wordBreak: 'break-word' } },
+      },
+    },
+  })
 )
+
+console.log(theme)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,25 +7,19 @@ import { useState } from 'react'
 import { LocaleContext } from '@/contexts/locale.context'
 import '../styles/default.css'
 import { theme } from '@/configs/theme.config'
-import { Titillium_Web } from 'next/font/google'
-
-// If loading a variable font, you don't need to specify the font weight
-const titilliumWeb = Titillium_Web({ subsets: ['latin'], weight: ['400', '600', '700'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [locale, setLocale] = useState<Locale>(DEFAULT_LOCALE)
   const router = useRouter()
 
   return (
-    <div className={titilliumWeb.className}>
-      <LocaleContext.Provider value={{ locale, setLocale }}>
-        <ThemeProvider theme={theme}>
-          <Layout>
-            <Component key={router.asPath} {...pageProps} />
-          </Layout>
-        </ThemeProvider>
-      </LocaleContext.Provider>
-    </div>
+    <LocaleContext.Provider value={{ locale, setLocale }}>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component key={router.asPath} {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </LocaleContext.Provider>
   )
 }
 
