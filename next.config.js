b9aba6b83c16f592337bf043acdf76bd9f0f2c82
Carmine Porricelli <carmine.porricelli@pagopa.it>
2023-07-14 09:37:54 +0200

    Improved layout shift

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-const withTM = require('next-transpile-modules')(['@pagopa/mui-italia'])
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
 /** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer(
-  withTM({ output: 'export', reactStrictMode: true, trailingSlash: true })
-)
+module.exports = withBundleAnalyzer({
+  output: 'export',
+  reactStrictMode: true,
+  trailingSlash: true,
+})
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,19 +7,25 @@ import { useState } from 'react'
 import { LocaleContext } from '@/contexts/locale.context'
 import '../styles/default.css'
 import { theme } from '@/configs/theme.config'
+import { Titillium_Web } from 'next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const titilliumWeb = Titillium_Web({ subsets: ['latin'], weight: ['400', '600', '700'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [locale, setLocale] = useState<Locale>(DEFAULT_LOCALE)
   const router = useRouter()
 
   return (
-    <LocaleContext.Provider value={{ locale, setLocale }}>
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Component key={router.asPath} {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </LocaleContext.Provider>
+    <div className={titilliumWeb.className}>
+      <LocaleContext.Provider value={{ locale, setLocale }}>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Component key={router.asPath} {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </LocaleContext.Provider>
+    </div>
   )
 }
 
