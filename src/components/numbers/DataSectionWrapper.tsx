57a07cf19a1f54e5060ef676279753f1a3adfb7f
DavideTurus <davide.turus@alten.it>
2023-10-19 14:58:05 +0200

    Added background color to the DataSectionWrapper

--- src/components/numbers/DataSectionWrapper.tsx
+++ src/components/numbers/DataSectionWrapper.tsx
@@ -3,27 +3,34 @@ import { Box, Container, Stack, Typography } from '@mui/material'
 type DataSectionWrapperProps = {
   title: string
   description: string
+  background?: 'white' | 'grey'
   children: React.ReactNode
 }
 
 export const DataSectionWrapper: React.FC<DataSectionWrapperProps> = ({
   title,
   description,
+  background = 'white',
   children,
 }) => {
   return (
-    <Container sx={{ py: 6 }}>
-      <Box sx={{ maxWidth: 620 }}>
-        <Typography variant="h4" component="h2">
-          {title}
-        </Typography>
-        <Typography sx={{ mt: 1 }} variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-      </Box>
-      <Stack sx={{ mt: 3 }} spacing={3}>
-        {children}
-      </Stack>
-    </Container>
+    <Box
+      component="section"
+      sx={{ backgroundColor: background === 'grey' ? 'background.default' : 'background.paper' }}
+    >
+      <Container sx={{ py: 6 }}>
+        <Box sx={{ maxWidth: 620 }}>
+          <Typography variant="h4" component="h2">
+            {title}
+          </Typography>
+          <Typography sx={{ mt: 1 }} variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </Box>
+        <Stack sx={{ mt: 3 }} spacing={3}>
+          {children}
+        </Stack>
+      </Container>
+    </Box>
   )
 }
