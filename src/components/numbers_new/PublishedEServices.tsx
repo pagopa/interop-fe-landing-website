9ea5eb9e7f9b34ac6e3ca39d7dc42ef1f8d20e96
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2023-11-08 12:04:55 +0100

    Broken commit

--- src/components/numbers_new/PublishedEServices.tsx
+++ src/components/numbers_new/PublishedEServices.tsx
@@ -8,51 +8,54 @@ import { ChartAndTableWrapper } from '@/components/numbers/ChartAndTableWrapper'
 import * as ECharts from 'echarts'
 import { formatThousands } from '@/utils/formatters.utils'
 import GovItLink from './GovItLink'
+import { pack, hierarchy } from 'd3-hierarchy'
+import { GridComponent } from 'echarts/components'
+
+// ECharts.core.use([GridComponent])
 
 const PublishedEServices = ({ mockData }) => {
-  console.log(mockData)
-  const chartOptions: ECharts.EChartsOption = React.useMemo(() => {
-    const filteredData = mockData.macroCategoriesPublishedEServicesMetric.filter(
-      (x) => x.publishedEServicesCount > 0
-    )
-    const max = Math.max(...filteredData.map((x) => x.publishedEServicesCount))
-    const scale = linearScale([0, max], [20, 100])
+  // const chartOptions: ECharts.EChartsOption = React.useMemo(() => {
+  //   const filteredData = mockData.macroCategoriesPublishedEServicesMetric.filter(
+  //     (x) => x.publishedEServicesCount > 0
+  //   )
+  //   const max = Math.max(...filteredData.map((x) => x.publishedEServicesCount))
+  //   const scale = linearScale([0, max], [20, 100])
 
-    return {
-      tooltip: {},
-      legend: {
-        show: true,
-        bottom: 0,
-        icon: 'circle',
-      },
-      series: [
-        {
-          type: 'graph',
-          layout: 'force',
-          animation: false,
-          data: filteredData.map((x) => ({
-            category: x.name,
-            name: x.name,
-            symbolSize: scale(x.publishedEServicesCount) / 0.5,
-            value: formatThousands(x.publishedEServicesCount),
-          })),
-          categories: filteredData.map(({ name }) => ({ name })),
-          roam: false,
-          label: {
-            show: true,
-            formatter: ({ value }) => formatThousands(value),
-          },
-          emphasis: {
-            scale: false,
-          },
-          force: {
-            repulsion: max / 10,
-            layoutAnimation: false,
-          },
-        },
-      ],
-    }
-  }, [])
+  //   return {
+  //     tooltip: {},
+  //     legend: {
+  //       show: true,
+  //       bottom: 0,
+  //       icon: 'circle',
+  //     },
+  //     series: [
+  //       {
+  //         type: 'graph',
+  //         layout: 'force',
+  //         animation: false,
+  //         data: filteredData.map((x) => ({
+  //           category: x.name,
+  //           name: x.name,
+  //           symbolSize: scale(x.publishedEServicesCount) / 0.5,
+  //           value: formatThousands(x.publishedEServicesCount),
+  //         })),
+  //         categories: filteredData.map(({ name }) => ({ name })),
+  //         roam: false,
+  //         label: {
+  //           show: true,
+  //           formatter: ({ value }) => formatThousands(value),
+  //         },
+  //         emphasis: {
+  //           scale: false,
+  //         },
+  //         force: {
+  //           repulsion: max / 10,
+  //           layoutAnimation: false,
+  //         },
+  //       },
+  //     ],
+  //   }
+  // }, [])
 
   const tableData: TableData = React.useMemo(() => {
     const head = ["Categoria d'ente", 'E-service pubblicati']
@@ -67,6 +70,67 @@ const PublishedEServices = ({ mockData }) => {
   const totalCount = mockData.publishedEServicesMetric.publishedEServicesCount
   const lastMonthCount = mockData.publishedEServicesMetric.lastMonthPublishedEServicesCount
   const variationPercentage = mockData.publishedEServicesMetric.variation
+
+  const bubbles = React.useMemo(() => {
+    const packing = pack().size([200, 200])
+    const computed = packing(
+      hierarchy({ children: mockData.macroCategoriesPublishedEServicesMetric }).sum(
+        (d) => d.publishedEServicesCount
+      )
+    )
+    return computed.leaves()
+  }, [mockData.macroCategoriesPublishedEServicesMetric])
+  //
+  const chartOptions = {
+    series: {
+      type: 'custom',
+      data: [{ x: 0, y: 0, r: 30 }],
+      xAxis: {
+        type: 'category',
+        data: [1],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      renderItem: (params, api) => {
+        console.log(params, api)
+
+        return {
+          cx: 0,
+          cy: 0,
+          size: 10,
+          type: 'circle',
+        }
+      },
+    },
+  }
+
+  // return {
+  //   tooltip: {},
+  //   visualMap: {
+  //     show: false,
+  //     min: 0,
+  //     max: 100,
+  //     dimension: 1,
+  //     inRange: {
+  //       color: ['#006edd', '#e0ffff'],
+  //     },
+  //   },
+  //   series: {
+  //     type: 'custom',
+  //     // renderItem: (params, api) => {
+  //     //   console.log(params, api)
+  //     //   return {
+  //     //     cx: 0,
+  //     //     cy: 0,
+  //     //     size: 10,
+  //     //     type: 'circle',
+  //     //   }
+  //     // },
+  //     coordinateSystem: null,
+  //     data: bubbles,
+  //   },
+  // }
 
   return (
     <Grid spacing={3} container>
