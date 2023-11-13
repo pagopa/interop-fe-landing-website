62e610a7a8c947ec17b12590e7eb4a91a7cb74de
DavideTurus <davide.turus@alten.it>
2023-11-13 09:55:38 +0100

    Implement hook to manage sort state

---
+++ src/hooks/useSort.ts
@@ -0,0 +1,15 @@
+import { SortBy } from '@/models/catalog.models'
+import { useQueryParam, withDefault, createEnumParam } from 'use-query-params'
+
+export function useSort() {
+  const SortByParam = createEnumParam<SortBy>([
+    'recent-asc',
+    'name-asc',
+    'name-desc',
+    'recent-desc',
+  ])
+
+  const [sortBy, setSortBy] = useQueryParam('order', withDefault(SortByParam, 'recent-asc'))
+
+  return { sortBy, setSortBy }
+}
