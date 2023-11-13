ab63f32a8abd168d9970a76eb3e7c94ce7574f5d
DavideTurus <davide.turus@alten.it>
2023-11-13 15:53:17 +0100

    Renamed workers and autocomplete options hooks

--- src/workers/producer-autocomplete-options.ts
+++ src/workers/producer-autocomplete-options.ts
@@ -3,7 +3,9 @@ import type { EServices } from '@/models/catalog.models'
 const sortingEventListener = (event: MessageEvent<EServices>) => {
   const eservices = event.data
 
-  const workerResult = Array.from(new Set(eservices.map((eservice) => eservice.producerName)))
+  const workerResult = Array.from(
+    new Set(eservices.map((eservice) => eservice.producerName))
+  ).sort()
   postMessage(workerResult)
 }
 
