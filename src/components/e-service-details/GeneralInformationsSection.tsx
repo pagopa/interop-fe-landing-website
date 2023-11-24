dec653f74fc027cecf85e13b2dd406d5e31c4200
DavideTurus <davide.turus@alten.it>
2023-11-24 16:04:37 +0100

    Added tooltip and new direction in InformationContainers

--- src/components/e-service-details/GeneralInformationsSection.tsx
+++ src/components/e-service-details/GeneralInformationsSection.tsx
@@ -1,7 +1,8 @@
 import type { EService } from '@/models/catalog.models'
-import { Divider, Skeleton, Stack, Typography } from '@mui/material'
+import { Divider, Skeleton, Stack, Tooltip, Typography } from '@mui/material'
 import { DetailsSection } from './DetailsSection'
 import { EServiceStateChip } from '../EServiceStateChip'
+import InfoIcon from '@mui/icons-material/Info'
 
 export const GeneralInformationsSection = ({ eservice }: { eservice: EService }) => {
   return (
@@ -13,15 +14,24 @@ export const GeneralInformationsSection = ({ eservice }: { eservice: EService })
         }
         spacing={2}
       >
-        <InformationContainer title="Versione attuale">
+        <InformationContainer
+          title="Versione attuale"
+          tooltip="Quando ci sono cambiamenti all’API e/o alla cornice amministrativa, è possibile pubblicare una nuova versione dello stesso e-service"
+        >
           <Typography variant="body2" sx={{ flex: 1, fontWeight: 600 }}>
             {eservice.activeDescriptor.version}
           </Typography>
         </InformationContainer>
-        <InformationContainer title="Stato della versione">
+        <InformationContainer
+          title="Stato della versione"
+          tooltip="Quando una versione di e-service viene sospesa, non è più possibile per i fruitori accedervi per ottenere dati"
+        >
           <EServiceStateChip state={eservice.activeDescriptor.state} />
         </InformationContainer>
-        <InformationContainer title="Tecnologia">
+        <InformationContainer
+          title="Tecnologia"
+          tooltip="L’architettura con la quale è costruita l’API che l’erogatore mette a disposizione dei fruitori, nel quadro del MODI. Può essere REST oppure SOAP"
+        >
           <Typography variant="body2" sx={{ flex: 1, fontWeight: 600 }}>
             {eservice.technology}
           </Typography>
@@ -41,15 +51,24 @@ export const GeneralInformationsSectionSkeleton: React.FC = () => {
         }
         spacing={2}
       >
-        <InformationContainer title="Versione attuale">
+        <InformationContainer
+          title="Versione attuale"
+          tooltip="Quando ci sono cambiamenti all’API e/o alla cornice amministrativa, è possibile pubblicare una nuova versione dello stesso e-service"
+        >
           <Typography variant="body2" sx={{ flex: 1 }}>
             <Skeleton width={10} />
           </Typography>
         </InformationContainer>
-        <InformationContainer title="Stato della versione">
+        <InformationContainer
+          title="Stato della versione"
+          tooltip="Quando una versione di e-service viene sospesa, non è più possibile per i fruitori accedervi per ottenere dati"
+        >
           <Skeleton variant="rectangular" sx={{ borderRadius: 10 }} width={55} height={26} />
         </InformationContainer>
-        <InformationContainer title="Tecnologia">
+        <InformationContainer
+          title="Tecnologia"
+          tooltip="L’architettura con la quale è costruita l’API che l’erogatore mette a disposizione dei fruitori, nel quadro del MODI. Può essere REST oppure SOAP"
+        >
           <Typography variant="body2" sx={{ flex: 1 }}>
             <Skeleton width={50} />
           </Typography>
@@ -61,15 +80,24 @@ export const GeneralInformationsSectionSkeleton: React.FC = () => {
 
 const InformationContainer = ({
   title,
+  tooltip,
   children,
 }: {
   title: string
+  tooltip: string
   children?: React.ReactNode
 }) => (
-  <Stack spacing={2} direction="row" alignItems="center" sx={{ flex: 1 }}>
-    <Typography variant="body2" sx={{ flex: 1 }}>
-      {title}
-    </Typography>
+  <Stack spacing={2} direction="column" alignItems="start" sx={{ flex: 1 }}>
+    <Stack direction="row" spacing={1}>
+      <Typography variant="body2" sx={{ flex: 1 }}>
+        {title}
+      </Typography>
+      <Tooltip arrow title={tooltip}>
+        <span tabIndex={0}>
+          <InfoIcon fontSize="small" color="primary" />
+        </span>
+      </Tooltip>
+    </Stack>
     <Stack justifyItems="center" alignItems="center" sx={{ flex: 1 }}>
       {children}
     </Stack>
