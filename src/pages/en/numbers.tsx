107069b26fcec2391d06532fcf80b4a6bb73e60a
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-01-12 14:10:49 +0100

    Added temporary courtesy page

---
+++ src/pages/en/numbers.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from 'next'
+import { getNumbersData } from '@/static'
+import Head from 'next/head'
+import { useLocaleContext } from '@/contexts'
+
+const ProjectPage: NextPage = () => {
+  const { locale } = useLocaleContext()
+  const data = getNumbersData(locale)
+
+  return (
+    <>
+      <Head>
+        <title>{data.title}</title>
+        <meta key="twitter:card" name="twitter:card" content="summary_large_image" />
+        <meta key="twitter:title" name="twitter:title" content={data.meta.title} />
+        <meta
+          key="twitter:description"
+          name="twitter:description"
+          content={data.meta.description}
+        />
+        <meta key="twitter:image" name="twitter:image" content={data.meta.imgTw} />
+        <meta key="og:locale" property="og:locale" content="it_IT" />
+        <meta key="og:type" property="og:type" content="website" />
+        <meta key="og:title" property="og:title" content={data.meta.title} />
+        <meta key="og:description" property="og:description" content={data.meta.description} />
+        <meta key="og:url" property="og:url" content={data.meta.url} />
+        <meta key="og:site_name" property="og:site_name" content={data.meta.sitename} />
+        <meta key="og:image" property="og:image" content={data.meta.imgFb} />
+      </Head>
+      <div>ciao</div>
+    </>
+  )
+}
+
+export default ProjectPage
