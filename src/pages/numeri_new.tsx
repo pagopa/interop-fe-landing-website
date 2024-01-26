d8fcae5b84962bab1d601ccde83e8f637cd54744
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-01-26 14:28:44 +0100

    Edited D to small d

--- src/pages/numeri_new.tsx
+++ src/pages/numeri_new.tsx
@@ -100,7 +100,7 @@ const PageTitles: React.FC<PageTitlesType> = ({ title, publishDate }) => {
         <Typography color="text.secondary" variant="body2" sx={{ lineHeight: 1 }}>
           I dati sono disponibili come .json e .csv su{' '}
           <Link href="https://dati.gov.it/view-dataset?organization=dtd" target="_blank">
-            Dati.gov.it <LaunchIcon fontSize="small" sx={{ position: 'relative', top: 6 }} />
+            dati.gov.it <LaunchIcon fontSize="small" sx={{ position: 'relative', top: 6 }} />
           </Link>
         </Typography>
         <Typography sx={{ mt: 1 }} component="p" color="text.secondary" variant="caption-semibold">
