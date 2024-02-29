a2b320ae1be084d564a398405b81c509396342a3
borgesis95 <salvatore.borgesi@pagopa.it>
2024-02-29 17:14:00 +0100

    implemented new filter on "enti che pubblicano più e-service"

--- src/components/numbers/TopProducers.tsx
+++ src/components/numbers/TopProducers.tsx
@@ -3,19 +3,350 @@ import { Stack, Typography, useTheme } from '@mui/material'
 import { TimeframeSelectInput } from '@/components/numbers/TimeframeSelectInput'
 import { ChartAndTableTabs, TableData } from './ChartAndTableTabs'
 import { ChartAndTableWrapper } from '@/components/numbers/ChartAndTableWrapper'
-import { Timeframe } from '@/models/numbers.models'
+import { MacroCategory, Timeframe } from '@/models/numbers.models'
 import * as ECharts from 'echarts'
 import { TopProducersMetric } from '@/models/numbers.models'
 import GovItLink from './GovItLink'
 import { formatThousands } from '@/utils/formatters.utils'
 import { BAR_CHART_NUMERIC_LABEL_COLOR, PRIMARY_BLUE } from '@/configs/constants.config'
 import { FiltersStack } from './FiltersStack'
+import { MacroCategorySelectInput } from './MacroCategorySelectInput'
+
+const mockData = {
+  lastSixMonths: [
+    {
+      id: '0',
+      name: 'Totale',
+      data: [
+        {
+          producerName: 'Comune di Milano',
+          count: 6189,
+        },
+        {
+          producerName: 'PagoPA S.p.A.',
+          count: 1209,
+        },
+        {
+          producerName: "Agenzia per L'Italia Digitale",
+          count: 996,
+        },
+        {
+          producerName: 'Comune di Firenze',
+          count: 1,
+        },
+      ],
+    },
+    {
+      id: '1',
+      name: 'Altre Pubbliche Amministrazioni locali',
+      data: [
+        {
+          producerName: "Agenzia per L'Italia Digitale",
+          count: 996,
+        },
+      ],
+    },
+    {
+      id: '2',
+      name: 'Aziende Ospedaliere e ASL',
+      data: [],
+    },
+    {
+      id: '3',
+      name: 'Comuni',
+      data: [
+        {
+          producerName: 'Comune di Milano',
+          count: 6189,
+        },
+        {
+          producerName: 'Comune di Firenze',
+          count: 1,
+        },
+      ],
+    },
+    {
+      id: '4',
+      name: 'Province e Città Metropolitane',
+      data: [],
+    },
+    {
+      id: '5',
+      name: 'Pubbliche Amministrazioni Centrali',
+      data: [],
+    },
+    {
+      id: '6',
+      name: 'Enti Nazionali di Previdenza ed Assistenza Sociale',
+      data: [],
+    },
+    {
+      id: '7',
+      name: 'Regioni e Province autonome',
+      data: [],
+    },
+    {
+      id: '8',
+      name: 'Consorzi e associazioni regionali',
+      data: [],
+    },
+    {
+      id: '9',
+      name: 'Scuole',
+      data: [],
+    },
+    {
+      id: '10',
+      name: 'Università e AFAM',
+      data: [],
+    },
+    {
+      id: '11',
+      name: 'Istituti di Ricerca',
+      data: [],
+    },
+    {
+      id: '12',
+      name: 'Stazioni Appaltanti e Gestori di pubblici servizi',
+      data: [
+        {
+          producerName: 'PagoPA S.p.A.',
+          count: 1209,
+        },
+      ],
+    },
+    {
+      id: '13',
+      name: 'Privati',
+      data: [],
+    },
+  ],
+  lastTwelveMonths: [
+    {
+      id: '0',
+      name: 'Totale',
+      data: [
+        {
+          producerName: 'Comune di Milano',
+          count: 6189,
+        },
+        {
+          producerName: 'PagoPA S.p.A.',
+          count: 1209,
+        },
+        {
+          producerName: "Agenzia per L'Italia Digitale",
+          count: 996,
+        },
+        {
+          producerName: 'Comune di Firenze',
+          count: 1,
+        },
+      ],
+    },
+    {
+      id: '1',
+      name: 'Altre Pubbliche Amministrazioni locali',
+      data: [
+        {
+          producerName: "Agenzia per L'Italia Digitale",
+          count: 996,
+        },
+      ],
+    },
+    {
+      id: '2',
+      name: 'Aziende Ospedaliere e ASL',
+      data: [],
+    },
+    {
+      id: '3',
+      name: 'Comuni',
+      data: [
+        {
+          producerName: 'Comune di Milano',
+          count: 6189,
+        },
+        {
+          producerName: 'Comune di Firenze',
+          count: 1,
+        },
+      ],
+    },
+    {
+      id: '4',
+      name: 'Province e Città Metropolitane',
+      data: [],
+    },
+    {
+      id: '5',
+      name: 'Pubbliche Amministrazioni Centrali',
+      data: [],
+    },
+    {
+      id: '6',
+      name: 'Enti Nazionali di Previdenza ed Assistenza Sociale',
+      data: [],
+    },
+    {
+      id: '7',
+      name: 'Regioni e Province autonome',
+      data: [],
+    },
+    {
+      id: '8',
+      name: 'Consorzi e associazioni regionali',
+      data: [],
+    },
+    {
+      id: '9',
+      name: 'Scuole',
+      data: [],
+    },
+    {
+      id: '10',
+      name: 'Università e AFAM',
+      data: [],
+    },
+    {
+      id: '11',
+      name: 'Istituti di Ricerca',
+      data: [],
+    },
+    {
+      id: '12',
+      name: 'Stazioni Appaltanti e Gestori di pubblici servizi',
+      data: [
+        {
+          producerName: 'PagoPA S.p.A.',
+          count: 1209,
+        },
+      ],
+    },
+    {
+      id: '13',
+      name: 'Privati',
+      data: [],
+    },
+  ],
+  fromTheBeginning: [
+    {
+      id: '0',
+      name: 'Totale',
+      data: [
+        {
+          producerName: 'Comune di Milano',
+          count: 6189,
+        },
+        {
+          producerName: 'PagoPA S.p.A.',
+          count: 1209,
+        },
+        {
+          producerName: "Agenzia per L'Italia Digitale",
+          count: 996,
+        },
+        {
+          producerName: 'Comune di Firenze',
+          count: 1,
+        },
+      ],
+    },
+    {
+      id: '1',
+      name: 'Altre Pubbliche Amministrazioni locali',
+      data: [
+        {
+          producerName: "Agenzia per L'Italia Digitale",
+          count: 996,
+        },
+      ],
+    },
+    {
+      id: '2',
+      name: 'Aziende Ospedaliere e ASL',
+      data: [],
+    },
+    {
+      id: '3',
+      name: 'Comuni',
+      data: [
+        {
+          producerName: 'Comune di Milano',
+          count: 6189,
+        },
+        {
+          producerName: 'Comune di Firenze',
+          count: 1,
+        },
+      ],
+    },
+    {
+      id: '4',
+      name: 'Province e Città Metropolitane',
+      data: [],
+    },
+    {
+      id: '5',
+      name: 'Pubbliche Amministrazioni Centrali',
+      data: [],
+    },
+    {
+      id: '6',
+      name: 'Enti Nazionali di Previdenza ed Assistenza Sociale',
+      data: [],
+    },
+    {
+      id: '7',
+      name: 'Regioni e Province autonome',
+      data: [],
+    },
+    {
+      id: '8',
+      name: 'Consorzi e associazioni regionali',
+      data: [],
+    },
+    {
+      id: '9',
+      name: 'Scuole',
+      data: [],
+    },
+    {
+      id: '10',
+      name: 'Università e AFAM',
+      data: [],
+    },
+    {
+      id: '11',
+      name: 'Istituti di Ricerca',
+      data: [],
+    },
+    {
+      id: '12',
+      name: 'Stazioni Appaltanti e Gestori di pubblici servizi',
+      data: [
+        {
+          producerName: 'PagoPA S.p.A.',
+          count: 1209,
+        },
+      ],
+    },
+    {
+      id: '13',
+      name: 'Privati',
+      data: [],
+    },
+  ],
+}
 
 const TopProducers = ({ data }: { data: TopProducersMetric }) => {
   const [timeframe, setTimeframe] = React.useState<Timeframe>('lastTwelveMonths')
+  const [macroCategory, setMacroCategory] = React.useState<MacroCategory['id']>('5')
+
   const [currentSearch, setCurrentSearch] = React.useState<{
     timeframe: Timeframe
-  }>({ timeframe })
+    macroCategory: MacroCategory['id']
+  }>({ timeframe, macroCategory })
 
   const fontFamily = useTheme().typography.fontFamily
   const textColorPrimary = useTheme().palette.text.primary
@@ -23,13 +354,13 @@ const TopProducers = ({ data }: { data: TopProducersMetric }) => {
   const mediaQuerySm = useTheme().breakpoints.values.sm
 
   const currentData = React.useMemo(() => {
-    return data[currentSearch.timeframe]
+    return mockData[currentSearch.timeframe].find((x) => x.id === currentSearch.macroCategory)
   }, [data, currentSearch])
 
   const chartOptions: ECharts.EChartsOption = React.useMemo(() => {
-    const sortedData = [...currentData].reverse()
-    const yAxisData = sortedData.map((x) => x.producerName)
-    const seriesData = sortedData.map((x) => x.count)
+    const sortedData = currentData?.data.reverse()
+    const yAxisData = sortedData?.map((x) => x.producerName)
+    const seriesData = sortedData?.map((x) => x.count)
 
     return {
       media: [
@@ -112,14 +443,14 @@ const TopProducers = ({ data }: { data: TopProducersMetric }) => {
 
   const tableData: TableData = React.useMemo(() => {
     const head = ['Erogatore', 'Numero di iscritti']
-    const body = currentData.map((x) => [x.producerName, formatThousands(x.count).toString()])
-
+    const body =
+      currentData?.data.map((x) => [x.producerName, formatThousands(x.count).toString()]) || []
     return { head, body }
   }, [currentData])
 
   const onSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault()
-    setCurrentSearch({ timeframe })
+    setCurrentSearch({ timeframe, macroCategory })
   }
 
   return (
@@ -130,6 +461,7 @@ const TopProducers = ({ data }: { data: TopProducersMetric }) => {
       <form onSubmit={onSubmit}>
         <FiltersStack>
           <TimeframeSelectInput value={timeframe} onChange={setTimeframe} />
+          <MacroCategorySelectInput value={macroCategory} onChange={setMacroCategory} />
         </FiltersStack>
       </form>
       <ChartAndTableTabs
