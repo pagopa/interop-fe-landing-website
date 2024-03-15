7dee8857327bf1451dc0f540122a833a79366ea6
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-03-15 10:59:00 +0100

    Edited anchor mobile

--- src/components/SectionSelectInput.tsx
+++ src/components/SectionSelectInput.tsx
@@ -1,6 +1,15 @@
-import { PRIMARY_BLUE } from '@/configs/constants.config'
 import { useHash } from '@/hooks/useHash'
-import { Box, InputLabel, Link, MenuItem, Select, SelectChangeEvent } from '@mui/material'
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  Link,
+  MenuItem,
+  Paper,
+  Select,
+  SelectChangeEvent,
+  Stack,
+} from '@mui/material'
 import React from 'react'
 type SectionSelectInput = {
   options: Array<{ ref: string; label: string; descr: string }>
@@ -14,45 +23,31 @@ export function SectionSelectInput({ options }: SectionSelectInput) {
   }
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      sx={{
-        backgroundColor: 'background.paper',
-        position: 'sticky',
-        top: 0,
-        width: '100%',
-        zIndex: 100,
-        paddingY: 1,
-        borderTop: 8,
-        borderBottom: 8,
-        borderColor: PRIMARY_BLUE,
-      }}
-    >
-      <Box sx={{ width: '95%', marginY: 2 }}>
-        <InputLabel
-          sx={{
-            transform: 'none',
-            position: 'static',
-            mb: 1,
-            fontSize: 16,
-            fontWeight: 400,
-            color: 'text.primary',
-          }}
-        >
-          Sezione visualizzata
-        </InputLabel>
-        <Select fullWidth size="small" value={hash} onChange={handleChange}>
-          {options.map(({ label, ref }, i) => (
-            <MenuItem value={ref} key={i}>
-              <Link underline="hover" color="inherit" href={`#${ref}`} key={i}>
-                {label}
-              </Link>
-            </MenuItem>
-          ))}
-        </Select>
-      </Box>
+    <Box sx={{ position: 'sticky', top: 0, width: '100%', zIndex: 100 }}>
+      <Paper elevation={3}>
+        <Stack direction="row" sx={{ py: 2, mx: 2 }}>
+          <FormControl sx={{ width: '100%' }}>
+            <InputLabel id="active-section">Sezione visualizzata</InputLabel>
+            <Select
+              id="abd"
+              labelId="active-section"
+              fullWidth
+              label="Sezione visualizzata"
+              size="small"
+              value={hash}
+              onChange={handleChange}
+            >
+              {options.map(({ label, ref }, i) => (
+                <MenuItem value={ref} key={i}>
+                  <Link underline="hover" color="inherit" href={`#${ref}`} key={i}>
+                    {label}
+                  </Link>
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Stack>
+      </Paper>
     </Box>
   )
 }
