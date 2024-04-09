2a647bfdf535929050b1e13e4c939c580907c6a4
borgesis95 <salvatore.borgesi@pagopa.it>
2024-04-09 10:40:33 +0200

    1) reduce charts TopProducersBySubscribers

--- src/components/numbers/TopProducersBySubscribers.tsx
+++ src/components/numbers/TopProducersBySubscribers.tsx
@@ -215,7 +215,7 @@ const TopProducersBySubscribers = ({ data }: { data: TopProducersBySubscribersMe
       </form>
       <ChartAndTableTabs
         chartOptions={chartOptions}
-        chartHeight={600}
+        chartHeight={380}
         tableData={tableData}
         info={Info}
         childrenPosition="top"
