ecda5c05775ed22023a0045f3bf8bf84ec0f9d8d
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2024-05-27 14:18:40 +0200

    Updated onetrust script link url

--- src/configs/tracking.ts
+++ src/configs/tracking.ts
@@ -14,8 +14,7 @@ type MixPanelCatalogReadEventProps = {
 
 export const { trackEvent, useTrackPageViewEvent } = initTracking<MixPanelEvent>({
   enabled: true,
-  oneTrustScriptUrl:
-    '/public/static/local_scripts/oneTrust_production/scripttemplates/otSDKStub.js',
+  oneTrustScriptUrl: '/static/local_scripts/oneTrust_production/scripttemplates/otSDKStub.js',
   domainScriptUrl: ONETRUST_DOMAIN_SCRIPT_ID,
   mixpanelToken: MIXPANEL_PROJECT_ID,
 })
