6cc31ea007cc4ea915e314cc7066b72815bcfcc1
Stefano Frontini <stefanofrontini75@gmail.com>
2024-05-27 11:13:28 +0200

    fix

--- src/components/numbers/MacroCategorySelectInput.tsx
+++ src/components/numbers/MacroCategorySelectInput.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import { MacroCategory } from '@/models/numbers.models'
 import { getLocalizedValue } from '@/utils/common.utils'
+import React from 'react'
 import { SelectInput } from './SelectInput'
 
 type MacroCategorySelectInputProps = {
@@ -14,19 +14,31 @@ export const MacroCategorySelectInput: React.FC<MacroCategorySelectInputProps> =
 }) => {
   const options: Array<{ value: MacroCategory['id']; label: MacroCategory['name'] }> = [
     { value: '0', label: 'Tutte' },
-    { value: '1', label: 'Altre Pubbliche Amministrazioni locali' },
-    { value: '2', label: 'Aziende Ospedaliere e ASL' },
-    { value: '3', label: 'Comuni' },
-    { value: '4', label: 'Province e città metropolitane' },
-    { value: '5', label: 'Pubbliche Amministrazioni Centrali' },
-    { value: '6', label: 'Enti Nazionali di Previdenza ed Assistenza Sociale' },
-    { value: '7', label: 'Regioni e Province autonome' },
-    { value: '8', label: 'Consorzi e associazioni regionali' },
-    { value: '9', label: 'Scuole' },
-    { value: '10', label: 'Università e AFAM' },
-    { value: '11', label: 'Istituti di Ricerca' },
-    { value: '12', label: 'Stazioni Appaltanti e Gestori di pubblici servizi' },
-    { value: '13', label: 'Enti privati' },
+    { label: 'Altre Pubbliche Amministrazioni locali', value: '1' },
+    { label: 'Aziende sanitarie locali e Strutture di ricovero', value: '2' },
+    { label: 'Comuni', value: '3' },
+    { label: 'Enti Nazionali di Previdenza ed Assistenza Sociale', value: '4' },
+    { label: 'Province e Citt\u00e0 Metropolitane', value: '5' },
+    { label: 'Pubbliche Amministrazioni Centrali', value: '6' },
+    { label: 'Regioni e Province autonome', value: '7' },
+    { label: 'Scuole', value: '8' },
+    { label: 'Stazioni Appaltanti e Gestori di pubblici servizi', value: '9' },
+    { label: 'Universit\u00e0 e AFAM', value: '10' },
+    { label: 'Enti privati', value: '11' },
+    { label: 'Non definito', value: '12' },
+    // { value: '1', label: 'Altre Pubbliche Amministrazioni locali' },
+    // { value: '2', label: 'Aziende Ospedaliere e ASL' },
+    // { value: '3', label: 'Comuni' },
+    // { value: '4', label: 'Province e città metropolitane' },
+    // { value: '5', label: 'Pubbliche Amministrazioni Centrali' },
+    // { value: '6', label: 'Enti Nazionali di Previdenza ed Assistenza Sociale' },
+    // { value: '7', label: 'Regioni e Province autonome' },
+    // { value: '8', label: 'Consorzi e associazioni regionali' },
+    // { value: '9', label: 'Scuole' },
+    // { value: '10', label: 'Università e AFAM' },
+    // { value: '11', label: 'Istituti di Ricerca' },
+    // { value: '12', label: 'Stazioni Appaltanti e Gestori di pubblici servizi' },
+    // { value: '13', label: 'Enti privati' },
   ]
 
   return (
