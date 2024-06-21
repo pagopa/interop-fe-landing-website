87703516a50ad324161e4c8ff5faa8f839162a85
Stefano Frontini <stefanofrontini75@gmail.com>
2024-06-21 11:49:41 +0200

    add url test site

--- src/static/data/numeri/it.tsx
+++ src/static/data/numeri/it.tsx
@@ -1,13 +1,15 @@
 import { HeadMetaProps } from '@/components'
-import { IMAGES_PATH, SITE_URL } from '@/configs/constants.config'
+import { IMAGES_PATH } from '@/configs/constants.config'
+
+const SITE_URL_TEST = 'https://interop-dashboard.netlify.app/numeri'
 
 const meta: HeadMetaProps = {
   title: 'I numeri della PDND | PDND Interoperabilità',
   description: 'Come sta andando PDND Interoperabilità',
   sitename: 'PDND Interoperabilità',
-  url: `${SITE_URL}/numeri`,
-  imgFb: `${SITE_URL}/${IMAGES_PATH}/social_interop_numeri_1200x630.png`,
-  imgTw: `${SITE_URL}/${IMAGES_PATH}/social_interop_numeri_800x420.png`,
+  url: `${SITE_URL_TEST}/numeri`,
+  imgFb: `${SITE_URL_TEST}/${IMAGES_PATH}/social_interop_numeri_1200x630.png`,
+  imgTw: `${SITE_URL_TEST}/${IMAGES_PATH}/social_interop_numeri_800x420.png`,
 }
 
 export const itNumbers = {
