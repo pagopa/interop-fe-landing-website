954405834bdf553d5b9500e9f68b9d62216b1b7a
Stefano Frontini <stefanofrontini75@gmail.com>
2024-06-21 11:26:57 +0200

    add new images

--- src/static/data/numeri/it.tsx
+++ src/static/data/numeri/it.tsx
@@ -1,13 +1,13 @@
-import { IMAGES_PATH, SITE_URL } from '@/configs/constants.config'
 import { HeadMetaProps } from '@/components'
+import { IMAGES_PATH, SITE_URL } from '@/configs/constants.config'
 
 const meta: HeadMetaProps = {
   title: 'I numeri della PDND | PDND Interoperabilità',
   description: 'Come sta andando PDND Interoperabilità',
   sitename: 'PDND Interoperabilità',
   url: `${SITE_URL}/numeri`,
-  imgFb: `${SITE_URL}/${IMAGES_PATH}/social_interop_01_1200x630.jpg`,
-  imgTw: `${SITE_URL}/${IMAGES_PATH}/social_interop_01_800x418.jpg`,
+  imgFb: `${IMAGES_PATH}/social_interop_numeri_1200x630.png`,
+  imgTw: `${IMAGES_PATH}/social_interop_numeri_800x420.png`,
 }
 
 export const itNumbers = {
