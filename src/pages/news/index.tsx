5f2bca0b5661ed1cf31af4ef520bbafdbe77f9b9
Ruggero Castagnola <ruggerocastagnola@gmail.com>
2025-06-23 10:36:24 +0200

    Added template for news archive page (#140)
    * Added template for news page
    
    * Removed unused import

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head'
 import React from 'react'
 import type { InferGetStaticPropsType } from 'next'
-import { getCatalogData, getCommonData, getNewsData } from '@/static'
+import { getCommonData, getNewsData } from '@/static'
 import { useLocaleContext } from '@/contexts'
-import { Dtd, PageBottomCta } from '@/components'
-import { Link } from '@mui/material'
-import { getLocalizedValue } from '@/utils/common.utils'
+import { Dtd } from '@/components'
+import { Box, Grid, Link, Typography } from '@mui/material'
+import { Stack } from '@mui/system'
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
+import { NewsItem } from './[id]'
 
 export async function getStaticProps() {
-  const news = getNewsData('it')
+  const { news } = getNewsData('it')
 
   return { props: { news } }
 }
@@ -16,7 +18,7 @@ export async function getStaticProps() {
 const NewsPage = ({ news }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { locale } = useLocaleContext()
   const commonData = getCommonData(locale)
-  const data = getCatalogData(locale)
+  const data = getNewsData(locale)
 
   return (
     <>
@@ -39,25 +41,46 @@ const NewsPage = ({ news }: InferGetStaticPropsType<typeof getStaticProps>) => {
         <meta key="og:image" property="og:image" content={data.meta.imgFb} />
       </Head>
 
-      <div>
-        {news.news.map((n, i) => {
-          return (
-            <div key={i}>
-              {n.id} - {n.title}
-              <Link href={`/news/${n.id}`} underline="always" variant="body2">
-                {getLocalizedValue({
-                  it: 'Leggi di più',
-                  en: 'Read more',
-                })}
-              </Link>
-            </div>
-          )
-        })}
-      </div>
+      <Box bgcolor="background.default" py={8} px={{ xs: 2, md: 14 }}>
+        <Typography variant="h4" component="h3" textAlign="center" pb={4}>
+          Aggiornamenti
+        </Typography>
+        <Grid item md={9}>
+          <Grid container spacing={3}>
+            {news.map((n, i) => (
+              <SingleNewsPreview key={i} {...n} />
+            ))}
+          </Grid>
+        </Grid>
+      </Box>
 
-      <PageBottomCta {...data.pageBottomCta} direction="horizontal" />
       <Dtd {...commonData.dtd} />
     </>
+  )
+}
+
+export const SingleNewsPreview = ({ title, date, id }: NewsItem) => {
+  return (
+    <Grid item md={4} my={2}>
+      <Typography color="text.secondary" fontSize={16} fontWeight={400} mb={2}>
+        {date}
+      </Typography>
+      <Typography variant="h6">{title}</Typography>
+      <Stack mt={2} direction="row" alignItems="center" color="primary.main">
+        <Link
+          color="primary.main"
+          underline="none"
+          textTransform="capitalize"
+          href={`news/${id}`}
+          title="Leggi la news"
+          fontSize={14}
+          fontWeight={400}
+        >
+          Leggi
+        </Link>
+        <ArrowRightAltIcon sx={{ color: 'inherit', fontSize: 18 }} />
+      </Stack>
+    </Grid>
   )
 }
 
